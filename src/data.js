66bffc433f7aca7d29f1a561ce60118551165856
kevin <kevin@squirrel.fr>
2021-03-29 13:34:20 +0400

    test

--- src/data.js
+++ src/data.js
@@ -2,13 +2,24 @@ import { v4 as uuidv4 } from 'uuid';
 function chillHop() {
   return [
     {
-      name: "Kinsfolk",
-      artist: "Aarigod",
-      cover: "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: true,
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=12997",
-      color: ["#6D7A3C", "#B6CE73"],
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
     },
     {
       name: "Flashback",
@@ -18,6 +29,25 @@ function chillHop() {
       active: false,
       audio: "https://mp3.chillhop.com/serve.php/?mp3=11225",
       color: ["#6759A8", "#A47DAD"],
+    },
+    {
+      name: "Kinsfolk",
+      artist: "Aarigod",
+      cover: "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12997",
+      color: ["#6D7A3C", "#B6CE73"],
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
     },
     {
       name: "Lax Incense",
@@ -29,6 +59,16 @@ function chillHop() {
       color: ["#712724", "#194F60"],
     },
     {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
       name: "ny90",
       artist: "Ezzy",
       cover: "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
@@ -38,23 +78,25 @@ function chillHop() {
       color: ["#B56A64", "#42438D"],
     },
     {
-      name: "Kinsfolk",
-      artist: "Aarigod",
-      cover: "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
       id: uuidv4(),
       active: false,
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=12997",
-      color: ["#6D7A3C", "#B6CE73"],
     },
     {
-      name: "Flashback",
-      artist: "Blue Wednesday, Shopan",
-      cover: "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg",
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false,
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=11225",
-      color: ["#6759A8", "#A47DAD"],
-    },
+    }
   ]
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./styles/app.scss";
+
+const g = "color:#00000;font-weight:bold;font-size:20px;";
+const hello =
+  "%c 👋 Hello, \n\n 🕵🏻‍♂️ Consulting. \n 👨🏻‍💻 Développement. \n 🤙 https://www.linkedin.com/in/kevin-grondin/";
+console.info(hello, g);
 
 ReactDOM.render(
   <React.StrictMode>
